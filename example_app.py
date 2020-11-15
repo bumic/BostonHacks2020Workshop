c13ee69813ee81f6bbb77a0db868d84effe6630f
Julius Frost <juliustfrost@gmail.com>
2020-11-14 20:11:40 -0500

    fix file data parameter

--- example_app.py
+++ example_app.py
@@ -2,7 +2,7 @@ import requests
 
 if __name__ == '__main__':
     # curl -O https://raw.githubusercontent.com/pytorch/serve/master/docs/images/kitten_small.jpg
-    files = {'upload_file': open('kitten_small.jpg', 'rb')}
+    files = {'data': open('kitten_small.jpg', 'rb')}
     url = 'http://127.0.0.1:8080/predictions/densenet161'
     response = requests.post(url, files=files)
     print(response.json())
